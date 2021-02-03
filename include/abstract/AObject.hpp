52589854b2763726f04a33102aecddca1554037d
Zacharie Corniere <cornierez@gmail.com>
2021-02-03 13:09:49 +0100

    chore(AObject): remove virtual key when unecessary

--- include/abstract/AObject.hpp
+++ include/abstract/AObject.hpp
@@ -14,21 +14,21 @@ public:
     {
     }
     virtual ~AObject(){};
-    virtual std::optional<std::string> getTextureName() { return sTexture; };
-    virtual void setRemove(bool vis) { bRemove = vis; };
-    virtual bool needRemove() { return bRemove; };
     virtual void onCollision(const std::unique_ptr<AObject> &){};
     virtual void onSceneryCollision() { this->setRemove(true); };
 
     virtual void update(const float) = 0;
 
+    inline std::optional<std::string> getTextureName()const { return sTexture; };
+    inline void setRemove(bool vis) { bRemove = vis; };
+    inline bool needRemove()const { return bRemove; };
     template <typename T = float>
-    Coords<T> getPosition() const
+    Coords<T> inline getPosition() const
     {
         return static_cast<Coords<T>>(fPosition);
     };
     template <typename T = float>
-    T getAngle() const
+    T inline getAngle() const
     {
         return static_cast<T>(fAngle);
     };
