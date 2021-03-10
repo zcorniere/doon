4f94ab20fe268838b031cecdad317e49cf6bab4f
Zacharie Corniere <cornierez@gmail.com>
2021-03-10 11:20:03 +0100

    feat(main): try catch function block

--- source/main.cpp
+++ source/main.cpp
@@ -10,7 +10,7 @@ Logger logger;
 __attribute__((constructor)) void ctor() { logger.start(); }
 __attribute__((destructor)) void dtor() { logger.stop(); }
 
-void sig_int_handler(int signal)
+void sig_int_handler(const int signal)
 {
     logger.err("SIGNAL") << "Received signal " << signal << ", exiting...";
     logger.endl();
@@ -18,17 +18,16 @@ void sig_int_handler(int signal)
 }
 
 int main()
-{
+try {
     std::signal(SIGINT, sig_int_handler);
     std::signal(SIGTERM, sig_int_handler);
 
-    try {
-        GameInstance game(WindowWidth, WindowHeight);
-        game.init();
-        game.run();
-    } catch (const std::exception &e) {
-        logger.err() << e.what();
-        logger.endl();
-    }
+    GameInstance game(WindowWidth, WindowHeight);
+    game.init();
+    game.run();
     return 0;
+} catch (const std::exception &e) {
+    logger.err() << e.what();
+    logger.endl();
+    return 1;
 }
