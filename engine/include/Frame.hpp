31ecc5dfd20ed0d8a17a60a8d0962295563348ef
Zacharie Corniere <cornierez@gmail.com>
2021-03-21 22:21:54 +0100

    chore(Frame): constexpr the inlined method

--- engine/include/Frame.hpp
+++ engine/include/Frame.hpp
@@ -17,22 +17,22 @@ public:
     void create(const Vector<unsigned> &, const Pixel = Color::Black);
     void rotate();
 
-    inline void setPixel(const Vector<unsigned> &pos, Pixel px)
+    constexpr void setPixel(const Vector<unsigned> &pos, Pixel px)
     {
         data.at(pos.y * size.x + pos.x) = std::move(px);
     }
-    inline void setPixel(const unsigned &x, const unsigned &y, Pixel px)
+    constexpr void setPixel(const unsigned &x, const unsigned &y, Pixel px)
     {
         data.at(y * size.x + x) = std::move(px);
     }
 
-    inline const uint8_t *getFramePtr() const { return (uint8_t *)data.data(); }
-    inline const Vector<unsigned> &getSize() const { return size; }
-    inline const Pixel &getPixel(const Vector<unsigned> &pos) const
+    constexpr const uint8_t *getFramePtr() const { return (uint8_t *)data.data(); }
+    constexpr const Vector<unsigned> &getSize() const { return size; }
+    constexpr const Pixel &getPixel(const Vector<unsigned> &pos) const
     {
         return data.at(pos.y * size.x + pos.x);
     }
-    inline const Pixel &getPixel(const unsigned x, const unsigned y) const
+    constexpr const Pixel &getPixel(const unsigned x, const unsigned y) const
     {
         return data.at(y * size.x + x);
     }
