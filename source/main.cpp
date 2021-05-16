421e23751428d9038756b25d6f13cb8d33492bd7
Zacharie Corniere <cornierez@gmail.com>
2021-05-16 21:27:08 +0200

    feat(getopt): added command line option for basemap and nb threads

--- source/main.cpp
+++ source/main.cpp
@@ -5,20 +5,52 @@
 #include "StorageManager.hpp"
 #include "ThreadManager.hpp"
 #include "Vector.hpp"
+#include <stdlib.h>
+#include <unistd.h>
 
 constexpr const char sAssetsPath[] = "./assets/";
+
 constexpr const char sBaseMap[] = "main";
+constexpr const int iDefaultThreadNb = 4;
 
 constexpr const unsigned WindowWidth = 1280;
 constexpr const unsigned WindowHeight = 960;
 constexpr const Vector<unsigned> uSize(WindowWidth, WindowHeight);
 
-int main()
+struct cmdOption {
+    const char *base_map = sBaseMap;
+    int nb_thread = iDefaultThreadNb;
+};
+
+cmdOption getCmdLineOption(int ac, char **av)
+{
+    cmdOption opt{};
+    int c;
+
+    while ((c = getopt(ac, av, "m:t:")) != -1) {
+        switch (c) {
+            case 'm': opt.base_map = optarg; break;
+            case 't': opt.nb_thread = atoi(optarg); break;
+            case '?': {
+                if (optopt == 'c')
+                    std::cerr << "Option -" << optopt << " requires an argument."
+                              << std::endl;
+                std::abort();
+            } break;
+            default: std::abort(); break;
+        }
+    }
+    return opt;
+}
+
+int main(int ac, char **av)
 try {
+    cmdOption opt = getCmdLineOption(ac, av);
+
     logger->start();
     storage_manager->load_directory(sAssetsPath);
-    thread_manager->start();
-    map_manager->start(sBaseMap);
+    thread_manager->start(opt.nb_thread);
+    map_manager->start(opt.base_map);
     render_manager->resize(uSize);
     GameInstance game(uSize);
 
